346f1ab999895ae47156d02d0999be2af498cc0c
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-10-24 13:51:28 +0100

    Pass tests for printing and updating board

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,13 +1,21 @@
-'user strict';
+'use strict';
 
 describe("Board", function() {
-  let board = new Board();
+  let board;
+  beforeEach(function() {
+    board = new Board();
+  });
 
   it("should print a board for the game", function() {
-    expect(board.initBoard()).toEqual("1 | 2 | 3\n" 
-                                     + "---------\n"
-                                     + "4 | 5 | 6\n"
-                                     + "---------\n"
-                                     + "7 | 8 | 9")
+    expect(board.printBoard()).toEqual("1 | 2 | 3\n" 
+                                    + "---------\n"
+                                    + "4 | 5 | 6\n"
+                                    + "---------\n"
+                                    + "7 | 8 | 9")
+  });
+
+  it("should update the board given a number and player name", function() {
+    board.updateBoard(8, "X");
+    expect(board._board).toEqual([[1, 2, 3], [4, 5, 6], [7, "X", 9]]);
   });
 });
--- src/board.js
+++ src/board.js
@@ -1,12 +1,19 @@
-'use strict'
+'use strict';
 
 class Board {
   constructor() {
-    this.board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    this._board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   }
 
-  initBoard() {
-    let rows = this.board.map(element => element.join(' | '));
+  printBoard() {
+    let rows = this._board.map(element => element.join(' | '));
     return rows.join('\n---------\n');
+  }
+
+  updateBoard(number, playerName) {
+    let index = number - 1;
+    let row = Math.floor(index / 3);
+    let col = index % 3;
+    this._board[row][col] = playerName;
   }
 }
